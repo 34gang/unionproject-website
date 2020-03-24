652bebe41a482578f2495a13dc9594fc78621bef
Rahman Hakim <rahmanhakim2435@mgila.com>
2020-03-25 03:27:02 +0700

    Final Changes Please

--- unionregisterweb/settings.py
+++ unionregisterweb/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c45bd47b6301fd89a399348d9a20cf6cdec1e25fdd3ca36b057d93e789d73e72'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['unionproject.herokuapp.com']
 
