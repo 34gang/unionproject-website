72ca49d7eeceb5cbb1b55bb6797dfebc628e9615
mochida <rahmanhakim2435@gmail.com>
2020-05-05 17:16:20 +0700

    Collected static

--- unionregisterweb/settings.py
+++ unionregisterweb/settings.py
@@ -141,7 +141,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'/static/')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
